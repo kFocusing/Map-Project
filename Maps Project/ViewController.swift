24ac0387f985e07733f9b709da92886321e58972
Danil <klymovd@gmail.com>
2022-02-15 11:39:50 +0200

    Initial Commit

---
+++ Maps Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Maps Project
+//
+//  Created by Danylo Klymov on 15.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
