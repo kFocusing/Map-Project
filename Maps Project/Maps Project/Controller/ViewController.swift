fe07ec1074a98a00829e5724cd7762e69c61ded1
Danil <klymovd@gmail.com>
2022-02-15 14:27:30 +0200

    Add GoogleMaps pod

--- Maps Project/Maps Project/Controller/ViewController.swift
+++ Maps Project/Maps Project/Controller/ViewController.swift
@@ -5,12 +5,14 @@
 //  Created by Danylo Klymov on 15.02.2022.
 //
 
+import GoogleMaps
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        GMSServices.provideAPIKey("AIzaSyBoXjLdkEsN8eTWEMHCajqLavHxc7-s3Ms")
         // Do any additional setup after loading the view.
     }
 
